df09452c005d1ba4f1b328a93aa4319719194bf7
jianbo <jjbob77@gmail.com>
2017-04-24 18:40:31 +0800

    add admin pain

---
+++ app/controllers/admin/jobs_controller.rb
@@ -0,0 +1,59 @@
+class Admin::JobsController < ApplicationController
+	before_action :authenticate_user!,only:[:new,:create,:update,:edit,:destroy]
+	before_action :set_admin_job,only:[:show,:edit,:update,:destroy]
+	before_action :require_is_admin
+
+	def index
+		@jobs =Job.all
+		
+	end
+
+	def new
+		@job =Job.new
+		
+	end
+
+	def show
+		
+	end
+
+	def edit
+		
+	end
+
+	def destroy
+		@job.destroy
+		redirect_to admin_jobs_path
+		
+	end
+
+	def create
+		@job=Job.new(job_params)
+		if @job.save
+			redirect_to admin_jobs_path
+		else
+			render :new
+			
+		end
+	end
+
+	def update
+		if @job.update(job_params)
+			redirect_to admin_jobs_path
+		else
+			render :edit
+		end
+		
+	end
+
+	private
+
+	def set_admin_job
+		@job =Job.find(params[:id])
+		
+	end
+
+	def job_params
+		params.require(:job).permit(:title,:description)
+	end
+end
---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,18 @@
+class JobsController < ApplicationController
+
+	def index
+		@jobs  =Job.all
+		
+	end
+
+	def new
+		@job =Job.new
+	end
+	
+	def show
+    @job = Job.find(params[:id])
+  	end
+
+
+
+end
