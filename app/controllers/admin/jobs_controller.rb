41df3d618c533cdb3b1d21117071e51197eb2eab
jianbo <jjbob77@gmail.com>
2017-05-13 06:37:04 +0800

    jobs index design

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -5,7 +5,15 @@ class Admin::JobsController < ApplicationController
 	layout "admin"
 
 	def index
-		@jobs =Job.all.recent
+		# @jobs =Job.all.recent
+		@jobs = case params[:order]
+            when 'by_lower_bound'
+              Job.published.order('wage_lower_bound DESC')
+            when 'by_upper_bound'
+              Job.published.order('wage_upper_bound DESC')
+            else
+              Job.published.recent
+            end
 		
 	end
 
